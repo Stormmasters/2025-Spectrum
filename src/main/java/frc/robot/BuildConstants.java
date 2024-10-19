10c1488b28db418a21028a9d07d5efd69ba15cb3
Allen Gregory <allengregoryiv@gmail.com>
2024-10-19 04:55:05 -0500

    updates

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "2025-SpectrumBeta";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = -1;
-    public static final String GIT_SHA = "UNKNOWN";
-    public static final String GIT_DATE = "UNKNOWN";
-    public static final String GIT_BRANCH = "UNKNOWN";
-    public static final String BUILD_DATE = "2024-10-19 02:36:17 EDT";
-    public static final long BUILD_UNIX_TIME = 1729319777847L;
-    public static final int DIRTY = 129;
+    public static final int GIT_REVISION = 2;
+    public static final String GIT_SHA = "aba8328b3ff89ec1704f382e7cf6e9b94fa2f01a";
+    public static final String GIT_DATE = "2024-10-19 02:43:45 EDT";
+    public static final String GIT_BRANCH = "main";
+    public static final String BUILD_DATE = "2024-10-19 05:52:18 EDT";
+    public static final long BUILD_UNIX_TIME = 1729331538042L;
+    public static final int DIRTY = 0;
 
     private BuildConstants() {}
 }
