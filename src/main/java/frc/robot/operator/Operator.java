3b9e56580d364637558c761c7a38717a05e87def
marcjuguilon <majuguilon25@mail.strakejesuit.org>
2024-11-19 16:53:32 -0600

    test commit

--- src/main/java/frc/robot/operator/Operator.java
+++ src/main/java/frc/robot/operator/Operator.java
@@ -7,12 +7,6 @@ import frc.spectrumLib.gamepads.Gamepad;
 import lombok.Getter;
 
 public class Operator extends Gamepad {
-
-    // Test commit
-
-    ///
-    //decreaseOffset_Ddp
-
     // Triggers, these would be robot states such as ampReady, intake, visionAim, etc.
     // If triggers need any of the config values set them in the constructor
     /*  A, B, X, Y, Left Bumper, Right Bumper = Buttons 1 to 6 in simulation */
