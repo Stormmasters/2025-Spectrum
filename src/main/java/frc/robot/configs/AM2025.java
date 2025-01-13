1ad351014f24d6d34e8d88026ee236ca565bc98a
Spectrum3847 <spectrum3847@gmail.com>
2025-01-12 18:02:55 -0600

    clean up
    remove all unused last year mechs

---
+++ src/main/java/frc/robot/configs/AM2025.java
@@ -0,0 +1,27 @@
+package frc.robot.configs;
+
+import frc.robot.Robot.Config;
+
+public class FM20235 extends Config {
+
+    // 2023.5 Off-season Robot
+
+    public FM20235() {
+        super();
+        swerve.configEncoderOffsets(-0.399170, -0.189209, 0.121826, -0.021973);
+
+        // Attached Mechanisms
+        pilot.setAttached(true);
+        operator.setAttached(true);
+        intake.setAttached(true);
+        shoulder.setAttached(true);
+        elbow.setAttached(true);
+
+        ampTrap.setAttached(false);
+        climber.setAttached(false);
+        elevator.setAttached(false);
+        feeder.setAttached(false);
+        launcher.setAttached(false);
+        pivot.setAttached(false);
+    }
+}
