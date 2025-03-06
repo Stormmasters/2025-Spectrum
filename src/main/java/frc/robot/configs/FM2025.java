46dbac819ba3c937b551792eab164fd86ebe7947
Aaron <spectrum3847@gmail.com>
2025-03-05 19:23:39 -0600

    swerve config and FM config file

--- src/main/java/frc/robot/configs/FM2025.java
+++ src/main/java/frc/robot/configs/FM2025.java
@@ -2,21 +2,21 @@ package frc.robot.configs;
 
 import frc.robot.Robot.Config;
 
-public class PM2025 extends Config {
+public class FM2025 extends Config {
 
-    public PM2025() {
+    public FM2025() {
         super();
-        swerve.configEncoderOffsets(0.490967, 0.410400, -0.208252, 0.422607);
+        swerve.configEncoderOffsets(-0.395264, 0.11499, 0.061279, 0.385742);
 
         // Attached Mechanisms
         pilot.setAttached(true);
         operator.setAttached(true);
         elevator.setAttached(false);
         shoulder.setAttached(false);
-        elbow.setAttached(true);
-        twist.setAttached(true);
-        intake.setAttached(true);
-        climb.setAttached(true);
+        elbow.setAttached(false);
+        twist.setAttached(false);
+        intake.setAttached(false);
+        climb.setAttached(false);
         groundIntake.setAttached(false);
     }
 }
